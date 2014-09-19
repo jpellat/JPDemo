a9acad6621353df4f7ac9098785e2bc81837b1f6
jordi Pellat <j.pellat@gmail.com>
2014-09-19 09:47:24 +0200

    Inject to Login VC the dependencies

--- JPDemo/JPAppDelegate.m
+++ JPDemo/JPAppDelegate.m
@@ -19,6 +19,8 @@
 
 #import "JPAppDelegate.h"
 #import "JPLoginViewController.h"
+#import "JPLoginHandler.h"
+#import "JPLastLoginStorage.h"
 
 @implementation JPAppDelegate
 
@@ -28,38 +30,22 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     
-    JPLoginViewController *loginViewController = [[JPLoginViewController alloc] init];
-    self.window.rootViewController = loginViewController;
+    self.window.rootViewController = [self loginViewController];
     
     [self.window makeKeyAndVisible];
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
+- (JPLoginViewController *)loginViewController
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
+    JPLoginHandler *loginaHandler = [[JPLoginHandler alloc] init];
+    JPLastLoginStorage *lastLoginStorage = [[JPLastLoginStorage alloc] init];
+    
+    JPLoginViewController *loginViewController = [[JPLoginViewController alloc] initWithLoginHandler:loginaHandler
+                                                                                    lastLoginStorage:lastLoginStorage];
+    
+    return loginViewController;
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
